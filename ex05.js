560aafbfaff21cbdad7c4eee38d955525acc25d5
Hackbright Student <info@hackbrightacademy.com>
2014-10-16 18:12:21 -0700

    Ex3 - 5 updated javascripts

--- ex05.js
+++ ex05.js
@@ -8,6 +8,7 @@ getMelon.addEventListener("click", function(e){
     e.preventDefault();
     alert("GetMelonWorked");
     var getMelonImage = document.getElementById("img")
+    alert("GetMelonImage Worked");
     getMelonImage.setAttribute("src", "http:/s/www.whataboutwatermelon.com/wp-content/uploads/2012/06/melon-4.jpg");
 
 })
